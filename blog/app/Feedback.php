2e544427a3da96c0a7e8e6cd55098773bdff09e4
MarcoMadeddu <marcomade99@gmail.com>
2020-06-23 17:09:51 +0200

    complete Index

--- blog/app/Feedback.php
+++ blog/app/Feedback.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    public function users()
+    public function user()
     {
-        return $this->hasOne('App\User');
+        return $this->belongsTo('App\User');
     }
 }
--- blog/resources/views/feedbacks/index.blade.php
+++ blog/resources/views/feedbacks/index.blade.php
@@ -3,7 +3,7 @@
     @foreach ($feedbacks as $feedback)
     <article>
         <h2>{{$feedback->title}}</h2>
- 
+        <h4>AUTHOR: {{$feedback->user->id}}</h4>
         <p>{{$feedback->description}}</p>
     </article>
     @endforeach
